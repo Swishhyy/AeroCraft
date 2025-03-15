34a68f66dbc972047ad541d91d6dad73224783a3
Swishhyy <dylan.minatrea@gmail.com>
2025-03-15 02:33:24 -0500

    Initial commit

---
+++ src/main/java/swishhyy/aerocraft/AeroCraftClient.java
@@ -0,0 +1,11 @@
+package swishhyy.aerocraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AeroCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/swishhyy/aerocraft/AeroCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package swishhyy.aerocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AeroCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
