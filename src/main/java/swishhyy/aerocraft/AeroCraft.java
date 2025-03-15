d3ef2628d661de11558f209ed74b841c6923c524
Swishhyy <dylan.minatrea@gmail.com>
2025-03-15 03:45:51 -0500

    Finished Basic Implementations of arrows
    Added all basic arrow types (no functionality yet)

--- src/main/java/swishhyy/aerocraft/AeroCraft.java
+++ src/main/java/swishhyy/aerocraft/AeroCraft.java
@@ -7,11 +7,11 @@ import org.slf4j.LoggerFactory;
 import swishhyy.aerocraft.Item.ModItems;
 
 public class AeroCraft implements ModInitializer {
-		public static final String MOD_ID = "aerocraft";
+	public static final String MOD_ID = "aerocraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		ModItems.registerModItems();
+		ModItems.initialize();
 	}
 }
--- src/main/java/swishhyy/aerocraft/Item/ModItems.java
+++ src/main/java/swishhyy/aerocraft/Item/ModItems.java
@@ -3,32 +3,62 @@ package swishhyy.aerocraft.Item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import swishhyy.aerocraft.AeroCraft;
 
+import java.util.function.Function;
+
 public class ModItems {
 
-    // Arrows
-    public static final Item WOODEN_ARROW = registerItem("wooden_arrow", new Item(new Item.Settings()));
-    public static final Item STONE_ARROW = registerItem("stone_arrow", new Item(new Item.Settings()));
-    public static final Item IRON_ARROW = registerItem("iron_arrow", new Item(new Item.Settings()));
-    public static final Item GOLD_ARROW = registerItem("gold_arrow", new Item(new Item.Settings()));
-    public static final Item DIAMOND_ARROW = registerItem("diamond_arrow", new Item(new Item.Settings()));
-    public static final Item EMERALD_ARROW = registerItem("emerald_arrow", new Item(new Item.Settings()));
-    public static final Item COPPER_ARROW = registerItem("copper_arrow", new Item(new Item.Settings()));
-    public static final Item QUARTZ_ARROW = registerItem("quartz_arrow", new Item(new Item.Settings()));
-    public static final Item REDSTONE_ARROW = registerItem("redstone_arrow", new Item(new Item.Settings()));
-    public static final Item ANCIENT_DEBRIS_ARROW = registerItem("ancient_debris_arrow", new Item(new Item.Settings()));
+    // 1) We’ll declare the items, but not initialize them here
+    public static Item WOODEN_ARROW;
+    public static Item STONE_ARROW;
+    public static Item IRON_ARROW;
+    public static Item GOLD_ARROW;
+    public static Item DIAMOND_ARROW;
+    public static Item EMERALD_ARROW;
+    public static Item COPPER_ARROW;
+    public static Item QUARTZ_ARROW;
+    public static Item REDSTONE_ARROW;
+    public static Item ANCIENT_DEBRIS_ARROW;
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(AeroCraft.MOD_ID, name), item);
+    // 2) A register method that matches the new docs style
+    private static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create a RegistryKey<Item> for "aerocraft:name"
+        RegistryKey<Item> itemKey = RegistryKey.of(
+                RegistryKeys.ITEM,
+                Identifier.of(AeroCraft.MOD_ID, name)
+        );
+
+        // Attach that key to the Item.Settings
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item with the game’s registry
+        Registry.register(Registries.ITEM, itemKey, item);
+        return item;
     }
 
-    public static void registerModItems() {
+    // 3) The initialize method (called in your main mod class) sets up the items
+    public static void initialize() {
         AeroCraft.LOGGER.info("Registering Mod Items for " + AeroCraft.MOD_ID);
 
+        // Create & register each arrow item
+        WOODEN_ARROW         = register("wooden_arrow",         Item::new, new Item.Settings());
+        STONE_ARROW          = register("stone_arrow",          Item::new, new Item.Settings());
+        IRON_ARROW           = register("iron_arrow",           Item::new, new Item.Settings());
+        GOLD_ARROW           = register("gold_arrow",           Item::new, new Item.Settings());
+        DIAMOND_ARROW        = register("diamond_arrow",        Item::new, new Item.Settings());
+        EMERALD_ARROW        = register("emerald_arrow",        Item::new, new Item.Settings());
+        COPPER_ARROW         = register("copper_arrow",         Item::new, new Item.Settings());
+        QUARTZ_ARROW         = register("quartz_arrow",         Item::new, new Item.Settings());
+        REDSTONE_ARROW       = register("redstone_arrow",       Item::new, new Item.Settings());
+        ANCIENT_DEBRIS_ARROW = register("ancient_debris_arrow", Item::new, new Item.Settings());
+
+        // Add them all to the COMBAT group
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
             entries.add(WOODEN_ARROW);
             entries.add(STONE_ARROW);
